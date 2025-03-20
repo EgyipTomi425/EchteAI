c34cdc1b1f8dcf97dc7dd352fe0c1486e10241a3
EgyipTomi425 <tomi04252002@gmail.com>
2025-03-20 08:44:08 +0000

    Setup.py

---
+++ Python/README.md
@@ -0,0 +1,1 @@
+Ez egy nagyon jó csomag!
--- Python/setup.py
+++ Python/setup.py
@@ -6,7 +6,7 @@ setup(
     author="EgyipTomi425",
     author_email="tomi04252002@gmail.com",
     description="Egy mesterséges intelligencia csomag",
-    long_description=open("README.md").read(),
+    long_description=open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
     long_description_content_type="text/markdown",
     url="https://github.com/EgyipTomi425/EchteAI",
     packages=find_packages(where="Python/EchteAI"),
